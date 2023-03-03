aefe2c2ccfbff0d22ba63ce9a51152e9b48d7575
YJK0805 <jerryko103@gmail.com>
2023-03-03 17:06:29 +0800

    3/3

--- UVa/Q10008 What's Cryptanalysis.cpp
+++ UVa/Q10008 What's Cryptanalysis.cpp
@@ -1,43 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long n;
-string s;
-struct co{
-	char ch;
-	int t;
-};
-co c[26];
-bool cmp(co a,co b){
-	if(a.t!=b.t){
-		return a.t>b.t;
-	}else{
-		return a.ch<b.ch;
+bool cmp(pair<int,char> a,pair<int,char> b){
+	if(a.first != b.first){
+		return a.first > b.first;
 	}
+	return a.second < b.second;
 }
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	for(int i=0;i<26;i++){
-		c[i].t=0;
-		c[i].ch=char(i+'A');
+	vector<pair<int,char>> c(26);
+	for(int i = 0;i < 26;i++){
+		c[i] = make_pair(0,char(i+'A'));
 	}
-	cin>>n;
+	long long n;
+	cin >> n;
 	cin.ignore();
-	for(int i=0;i<n;i++){
+	for(int i = 0;i < n;i++){
+		string s;
 		getline(cin,s);
-		for(int j=0;j<s.size();j++){
-			if(s[j]>='a'&&s[j]<='z'){
-				c[s[j]-'a'].t++;
+		for(int j = 0;j < s.size();j++){
+			if(s[j] >= 'a' && s[j] <= 'z'){
+				c[s[j] - 'a'].first++;
 			}
-			if(s[j]>='A'&&s[j]<='Z'){
-				c[s[j]-'A'].t++;
+			if(s[j] >= 'A' && s[j] <= 'Z'){
+				c[s[j] - 'A'].first++;
 			}
 		}
 	}
-	sort(c,c+26,cmp);
-	for(int i=0;i<26;i++){
-		if(c[i].t!=0){
-			cout<<c[i].ch<<" "<<c[i].t<<"\n";
+	sort(c.begin(),c.end(),cmp);
+	for(int i = 0;i < 26;i++){
+		if(c[i].first != 0){
+			cout << c[i].second << " " << c[i].first << "\n";
 		}else{
 			break;
 		} 
