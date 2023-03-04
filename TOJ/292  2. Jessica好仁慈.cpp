30386edac4c451408fd09b16ecf0086b213ad3a7
YJK0805 <jerryko103@gmail.com>
2023-03-04 20:54:09 +0800

    3/4

--- TOJ/292  2. Jessica好仁慈.cpp
+++ TOJ/292  2. Jessica好仁慈.cpp
@@ -1,30 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    const double EPS=1e-7;
-    long long n=0,i,j,len,a,b,be=0,x;
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    long long a,b;
     string c;
-    cin>>a>>b>>c;
-    if(c=="0"){
-    	cout<<0<<"\n";
+    cin >> a >> b >> c;
+    if(c == "0"){
+    	cout << 0 << "\n";
 	}else{
-	    len=c.length();
-	    for(i=len-1,j=0;i>=0;i--,j++){
-	        x=pow(a,i)+EPS;
-	        x/=1;
-	        be+=(c[j]-'0')*x;
+	    long long num = 0,p = 1;
+	    for(int i = c.size() - 1;i >= 0;i--){
+	    	num += ((c[i] - '0') * p);
+	    	p *= a;
 	    }
-	    while(be>=pow(b,n)){
-	        n++;
+	    string ans = "";
+	    while(num){
+	    	ans = (char)(num % b + '0') + ans;
+	    	num /= b;
 	    }
-	    n-=1;
-	    for(i=n;i>=0;i--){
-	        x=pow(b,i)+EPS;
-	        x/=1;
-	        cout<<be/x;
-	        be=be%x;
-	    }
-	    cout<<"\n";
+	    cout << ans << "\n";
 	}
     return 0;
 }
